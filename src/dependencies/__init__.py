49bea3dfe0bca88f39fe654d58928b2f2afaf750
raghadsalameh1 <raghadsalameh1@gmail.com>
2022-09-28 22:58:21 +0300

    Add new user meta data endpoint

--- src/dependencies/__init__.py
+++ src/dependencies/__init__.py
@@ -10,3 +10,5 @@ cred = credentials.Certificate(settings.FIREBASE_ADMIN_CRED_PATH)
 firbase_admin = firebase_admin.initialize_app(cred)
 firbase = pyrebase.initialize_app(
     json.load(open(settings.FIREBASE_CONFIG_PATH)))
+
+db = firbase.database()
--- src/routes/users/views.py
+++ src/routes/users/views.py
@@ -1,3 +1,19 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, status
+from starlette.responses import Response
+
+from src.routes.users.handler import add_user
+from src.routes.users.schema import UserMetaData
 
 router = APIRouter()
+
+
+@router.post('',
+             status_code=200,
+             responses={401: {'description': 'UNAUTHORIZED'},
+                        400: {'description': 'BAD REQUEST'}})
+async def Add_new_participant(user: UserMetaData):
+    result = add_user(dict(user))
+    if result:
+        return Response(status_code=status.HTTP_200_OK)
+    else:
+        return Response(status_code=status.HTTP_400_BAD_REQUEST)
