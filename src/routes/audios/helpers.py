9e6200274eaa2258af6a521b5170a140920b9976
raghadsalameh1 <raghadsalameh1@gmail.com>
2022-10-03 19:20:00 +0300

    Limit video length and standarize the audio files by converting it to wav

--- src/routes/audios/helpers.py
+++ src/routes/audios/helpers.py
@@ -1,13 +1,17 @@
-from typing import BinaryIO
+from tempfile import NamedTemporaryFile
+from typing import IO, BinaryIO, Tuple
+
+from pydub import AudioSegment
 
 from src.dependencies import minio_client
 from src.settings import settings
 
 
-def upload_file(new_file_name: str, audio_file: int) -> bool:
+def upload_file(new_file_name: str, audio_file_path: str) -> bool:
+    """ Upload file to minio server"""
     try:
         minio_client.fput_object(
-            settings.MINIO_BUCKET_NAME, new_file_name, audio_file
+            settings.MINIO_BUCKET_NAME, new_file_name, audio_file_path
         )
         return True
     except Exception:
@@ -15,7 +19,10 @@ def upload_file(new_file_name: str, audio_file: int) -> bool:
         return False
 
 
-def valid_file_size(file: BinaryIO, file_size_limit: int) -> bool:
-    # TODO
-    valid = True
-    return valid
+def process_audio(file: BinaryIO) -> Tuple[int, str]:
+    """ Get the lenght of audio and convert it to wav"""
+    temp: IO = NamedTemporaryFile(delete=False)
+    audio_seg = AudioSegment.from_file(file)
+    audio_seg.export(temp, format='wav')
+    duration_ms = len(audio_seg)
+    return duration_ms, temp.name
--- src/routes/audios/views.py
+++ src/routes/audios/views.py
@@ -3,11 +3,10 @@ import uuid
 from fastapi import APIRouter, Depends, File, HTTPException, UploadFile, status
 
 from src.routes.audios.handler import add_audio
-from src.routes.audios.helpers import upload_file, valid_file_size
+from src.routes.audios.helpers import process_audio, upload_file
 from src.routes.audios.schema import (AudioMetaData, AudioMetaDataIn,
                                       UploadOutSchema)
 from src.routes.users.handler import get_user
-from src.routes.users.schema import PlatformEnum
 
 router = APIRouter()
 
@@ -20,13 +19,6 @@ router = APIRouter()
                         404: {'description': 'NOT FOUND'}})
 async def Add_new_audio(audio_meta_data: AudioMetaDataIn = Depends(),
                         audio_file: UploadFile = File(...)):
-    audio_part = audio_file.filename.split('.')
-    if len(audio_part) != 2:
-        raise HTTPException(
-            status_code=status.HTTP_400_BAD_REQUEST,
-            detail="audio file name shoudn't contains dots",
-        )
-
     if not audio_file.content_type.startswith('audio'):
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST,
@@ -39,36 +31,27 @@ async def Add_new_audio(audio_meta_data: AudioMetaDataIn = Depends(),
             status_code=status.HTTP_404_NOT_FOUND,
             detail=f'There is no client with id {audio_meta_data.client_id}',
         )
-    # TODO depend on the platform check the extension of the audio file
-    audio_extension = audio_part[1]
-    if user.platform == PlatformEnum.IOS:
-        if audio_extension != 'aac':
-            raise HTTPException(
-                status_code=status.HTTP_400_BAD_REQUEST,
-                detail='only acc format is supported for IOS',
-            )
 
-    # TODO: limit file size and throw exception if it's too big
-    max_upload_size = 2 * 1024 * 1024       # 2MB
-    valid = valid_file_size(audio_file.file, max_upload_size)
-    if not valid:
+    # limit audio file length and throw exception if it's too long
+    # convert audio file to wav
+    max_audio_length = 3 * 60000       # 3 min
+    audio_length_ms, wav_file = process_audio(audio_file.file)
+    if audio_length_ms > max_audio_length:
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST,
-            detail="The audio file shouldn't exced 2MB",
+            detail="The audio file shouldn't exced 3 Min",
         )
-    # TODO verified the content of the audio file using PyDub
-    # upload the file to s3
+
     file_id = uuid.uuid4()
-    new_file_name = str(file_id)+'.'+audio_extension
-    # check that the file is uploaded successfully
-    if upload_file(new_file_name, audio_file.file.fileno()):
-        # calculate file size in ms. maybe we can do that in the begaining
-        duration_ms = 10
+    new_file_name = str(file_id)+'.wav'
+
+    # upload the file to s3 and check that the file is uploaded successfully
+    if upload_file(new_file_name, wav_file):
         # send Meta data to firbase
         audio_meta_data = AudioMetaData(client_id=audio_meta_data.client_id,
                                         sentence=audio_meta_data.sentence,
                                         audio_file_name=new_file_name,
-                                        duration_ms=duration_ms)
+                                        duration_ms=audio_length_ms)
         if add_audio(file_id, dict(audio_meta_data)):
             return {'file_name': new_file_name}
         else:
