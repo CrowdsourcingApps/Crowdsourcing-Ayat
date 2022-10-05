6dd3d12be7a74d75d40740869969a7692f1d0a2f
raghadsalameh1 <raghadsalameh1@gmail.com>
2022-10-05 17:37:41 +0300

    Add volume for logging and prevent health check from being logged (#10)

--- src/main.py
+++ src/main.py
@@ -13,17 +13,22 @@ app = FastAPI()
 
 @app.middleware('http')
 async def log_requests(request, call_next):
-    idem = ''.join(random.choices(string.ascii_uppercase + string.digits, k=6))
-    logger.info(f'rid={idem} start - "{request.method} {request.url.path}"')
-    start_time = time.time()
+    # do not register healthcheck
+    health_check = request.url.path.__contains__('docs')
+    if not health_check:
+        idem = ''.join(random.choices(string.ascii_uppercase + string.digits,
+                                      k=6))
+        logger.info(f'rid={idem} start - "{request.method}"'
+                    '"{request.url.path}"')
+        start_time = time.time()
 
     response = await call_next(request)
 
-    process_time = (time.time() - start_time) * 1000
-    formatted_process_time = '{0:.2f}'.format(process_time)
-    logger.info(f'rid={idem} completed_in={formatted_process_time}ms'
-                f' status_code={response.status_code}')
-
+    if not health_check:
+        process_time = (time.time() - start_time) * 1000
+        formatted_process_time = '{0:.2f}'.format(process_time)
+        logger.info(f'rid={idem} completed_in={formatted_process_time}ms'
+                    f' status_code={response.status_code}')
     return response
 
 app.add_middleware(
--- src/settings/logging.py
+++ src/settings/logging.py
@@ -1,9 +1,11 @@
 import logging
+from pathlib import Path
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 ch = logging.StreamHandler()
-fh = logging.handlers.RotatingFileHandler('server.log',
+Path('src/log_files').mkdir(parents=True, exist_ok=True)
+fh = logging.handlers.RotatingFileHandler('src/log_files/server.log',
                                           mode='a',
                                           maxBytes=100*1024,
                                           backupCount=3)
