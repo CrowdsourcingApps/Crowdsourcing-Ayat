e6938f9be4a204209ef7d698fc562d84c33bf385
raghadsalameh1 <raghadsalameh1@gmail.com>
2022-11-05 20:39:53 +0300

    Add Create date filed to UserMetaData

--- src/routes/users/views.py
+++ src/routes/users/views.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter, status
 from starlette.responses import Response
 
 from src.routes.users.handler import add_user
-from src.routes.users.schema import UserMetaData
+from src.routes.users.schema import UserMetaData, UserMetaDataCreate
 
 router = APIRouter()
 
@@ -11,8 +11,13 @@ router = APIRouter()
              status_code=200,
              responses={401: {'description': 'UNAUTHORIZED'},
                         400: {'description': 'BAD REQUEST'}})
-async def Add_new_participant(user: UserMetaData):
-    result = add_user(dict(user))
+async def Add_new_participant(userIn: UserMetaDataCreate):
+    user = UserMetaData(**userIn.dict())
+    user_dict = dict(user)
+    ''' The next line is to avoid  the TypeError:
+                "Object of type datetime is not JSON serializable"'''
+    user_dict['create_date'] = str(user_dict['create_date'])
+    result = add_user(user_dict)
     if result:
         return Response(status_code=status.HTTP_200_OK)
     else:
