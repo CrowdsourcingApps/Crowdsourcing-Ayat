1cd794b3b2e1fdacd3df24643afa2a966e21d952
raghadsalameh1 <raghadsalameh1@gmail.com>
2022-11-07 16:09:48 +0300

    Removing silence from audio

--- src/routes/audios/helpers.py
+++ src/routes/audios/helpers.py
@@ -2,6 +2,7 @@ from tempfile import NamedTemporaryFile
 from typing import IO, BinaryIO, Tuple
 
 from pydub import AudioSegment
+from pydub.silence import split_on_silence
 
 from src.dependencies import minio_client
 from src.settings import settings
@@ -25,8 +26,19 @@ def process_audio(file: BinaryIO) -> Tuple[int, str]:
     """ Get the lenght of audio and convert it to standarized wav"""
     temp: IO = NamedTemporaryFile(delete=False)
     audio_seg = AudioSegment.from_file(file)
-    audio_seg = audio_seg.set_channels(1)
-    audio_seg = audio_seg.set_frame_rate(16000)
-    audio_seg.export(temp, format='wav')
+    audio_seg = audio_seg.set_channels(1)  # 1 channel (mono)
+    audio_seg = audio_seg.set_frame_rate(16000)  # frequency 16khz
+    audio_seg = audio_seg.set_sample_width(2)  # bit depth = 2 Byte = 16 bits
+    # A suitable value for silence_thresh might be around -50 to -35
+    audio_chunks = split_on_silence(audio_seg,
+                                    min_silence_len=100,
+                                    silence_thresh=-45,
+                                    keep_silence=50
+                                    )
+    # Putting the file back together
+    combined = AudioSegment.empty()
+    for chunk in audio_chunks:
+        combined += chunk
+    combined.export(temp, format='wav')
     duration_ms = len(audio_seg)
     return duration_ms, temp.name
