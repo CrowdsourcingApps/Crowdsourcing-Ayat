e2b0e667fd5883f3c07735cd0da1935eab7132d0
raghadsalameh1 <raghadsalameh1@gmail.com>
2023-01-18 04:08:41 +0300

    Add register endpoint

--- src/routes/auth/schema.py
+++ src/routes/auth/schema.py
@@ -3,10 +3,17 @@ from pydantic import BaseModel
 
 class Token(BaseModel):
     access_token: str
-    # refresh_token: str
-    # token_type: str
+    refresh_token: str
 
 
 class UserOutSchema(BaseModel):
     user_id: str
     user_email: str
+    validate_correctness_accuracy: int = None
+    transcription_accuracy: int = None
+    validate_transcription_accuracy: int = None
+
+
+class UserInSchema(BaseModel):
+    user_email: str
+    password: str
--- src/routes/auth/views.py
+++ src/routes/auth/views.py
@@ -3,7 +3,8 @@ from fastapi.security import OAuth2PasswordRequestForm
 
 from src.dependencies import firbase
 from src.dependencies.auth import firebase_authentication
-from src.routes.auth.schema import Token, UserOutSchema
+from src.routes.auth.handler import add_participant
+from src.routes.auth.schema import Token, UserInSchema, UserOutSchema
 from src.settings.logging import logger
 
 router = APIRouter()
@@ -29,7 +30,46 @@ async def login_for_access_token(
             headers={'WWW-Authenticate': 'Bearer'},
         )
     jwt = user['idToken']
-    return {'access_token': jwt}
+    refresh_token = user['refreshToken']
+    response = Token(access_token=jwt, refresh_token=refresh_token)
+    return response
+
+
+@router.post(
+    '/register',
+    response_model=Token,
+    status_code=200,
+    responses={400: {'description': 'BAD REQUEST'}},
+)
+async def sign_up(
+        form_data: UserInSchema):
+    try:
+        # create new user
+        user = firbase.auth().create_user_with_email_and_password(
+            form_data.user_email, form_data.password)
+        # Add the new user to the participants node with additional data
+        user_id = user['localId']
+        refresh_token = user['refreshToken']
+        participant = UserOutSchema(user_id=user_id,
+                                    user_email=form_data.user_email)
+        participant_dict = dict(participant)
+        if add_participant(participant_dict):
+            jwt = user['idToken']
+            response = Token(access_token=jwt, refresh_token=refresh_token)
+            return response
+        else:
+            raise HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST,
+                detail='participant data was not saved successfully',
+            )
+    except Exception as ex:
+        logger.exception('[Firebase] - Sign up with Firebase failed:'
+                         f' {ex}')
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail='Email is already exists',
+            headers={'WWW-Authenticate': 'Bearer'},
+        )
 
 
 @router.get(
