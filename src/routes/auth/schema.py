e7090ed4d1d2c3b60744f45b356a676fcf0ddf82
raghadsalameh1 <raghadsalameh1@gmail.com>
2023-01-22 15:30:09 +0300

    Edit participants schema with adding accuracy data

--- src/routes/auth/schema.py
+++ src/routes/auth/schema.py
@@ -8,12 +8,26 @@ class Token(BaseModel):
 
 class UserUpdateSchema(BaseModel):
     user_email: str
-    validate_correctness_accuracy: int = None
-    transcription_accuracy: int = None
-    validate_transcription_accuracy: int = None
-    number_validate_correctness_tasks: int = 0
-    number_transcription_tasks: int = 0
-    number_validate_transcription: int = 0
+
+    # number of validate correctness tasks that user solved
+    validate_correctness_solved_tasks_no: int = 0
+
+    # number of transcription tasks that users solved
+    transcription_solved_tasks_no: int = 0
+
+    # number of validate transcription tasks that users solved
+    validate_transcription_solved_tasks_no: int = 0
+
+    # accuracy for each type of task =
+    # task type_solved_control_correct_no/task type_solved_control_no
+    validate_correctness_solved_control_no: int = 0
+    validate_correctness_solved_control_correct_no: int = 0
+
+    transcription_solved_control_no: int = 0
+    transcription_solved_control_correct_no: int = 0
+
+    validate_transcription_solved_control_no: int = 0
+    validate_transcription_solved_control_correct_no: int = 0
 
 
 class UserOutSchema(UserUpdateSchema):
